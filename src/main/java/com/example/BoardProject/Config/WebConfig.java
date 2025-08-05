b1cd5256d43d2f24898cc2a691f7702375f4b3b4
marine64737 <tkdgh01248@naver.com>
2025-08-05 22:42:33 +0900

    Update WebConfig.java

--- src/main/java/com/example/BoardProject/Config/WebConfig.java
+++ src/main/java/com/example/BoardProject/Config/WebConfig.java
@@ -12,6 +12,8 @@ public class WebConfig implements WebMvcConfigurer {
     public void addResourceHandlers(ResourceHandlerRegistry registry) { // 파일 업로드 관련 함수, 이해가 불확실하여 추후 재학습
         registry.addResourceHandler("/js/**") //파일
                 .addResourceLocations("file:"+System.getProperty("user.dir")+"/src/main/resources/static/js");
+        registry.addResourceHandler("/js/**") //파일
+                .addResourceLocations("classpath:/static/js/");
         registry.addResourceHandler("/upload/**") //파일
                 .addResourceLocations("file:" + System.getProperty("user.dir") + "/upload/");
     }
